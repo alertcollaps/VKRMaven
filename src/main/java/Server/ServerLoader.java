1d3a105f83d5af928d636e02b81661c618a5c65b
borisov.aleksey18 <borisov.aleksey18@wb.ru>
2022-03-13 18:48:31 +0300

    Deploy ggg

--- src/main/java/Server/ServerLoader.java
+++ src/main/java/Server/ServerLoader.java
@@ -39,8 +39,8 @@ public class ServerLoader {
 
     private static void start(){
         try {
-            System.out.println(Integer.parseInt(System.getenv().get("PORT")));
-            server = new ServerSocket(Integer.parseInt(System.getenv().get("PORT")));
+            System.out.println(Integer.parseInt(System.getenv("PORT")));
+            server = new ServerSocket(Integer.parseInt(System.getenv("PORT")));
 
         } catch (IOException e) {
             e.printStackTrace();
